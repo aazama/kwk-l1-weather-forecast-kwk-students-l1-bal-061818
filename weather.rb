e85e721345e639d95e1dd10ae1f05598cf549abe
ava azama <avaazama@gmail.com>
2018-06-21 18:43:42 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -1,6 +1,6 @@
 # Your methods should take in a hash as an argument - here's an example of what the hash will look like - the methods should take this hash as an argument and return the desired output.
 
-weather = {
+$weather = {
 	:city => "New York",
 	:description => "Partly Cloudy",
 	:temperature_farenheit => 66.3
@@ -8,13 +8,13 @@ weather = {
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash.
 def location(weather)
-	return weather [:city]
+	return $weather [:city]
 
 end
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash.
 def description(weather)
-	return weather [:description]
+	return $weather [:description]
 end
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal.
@@ -23,13 +23,13 @@ end
 #4. Write a method that takes in the weather hash as an argument.
 # You'll want to use the each method to iterate over the hash and print out
 def weather_report (weather)
-	return "The weather in #{location(weather)} is {description(weather)}"
+	return "The weather in #{location($weather)} is {description($weather)}"
 end
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
 def city_weather_info (weather)
-	puts "The city is #{weather[:city]}."
-	puts "The descriptionis #{description(weather)}."
+	puts "The city is #{$weather[:city]}."
+	puts "The descriptionis #{description($weather)}."
 	puts "The temperature is #{weather[temperature_farenheit]}."
 end
 # BONUS:  Write a method that takes in the weather hash as an argument and converts the farenheit temperature to celcius. Have the method round your answer to two decimal places. Is there a method we can use to round in Ruby?
