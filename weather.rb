10fc71b0da88e9138e07fdaa58a38a362e3c1510
ava azama <avaazama@gmail.com>
2018-06-21 18:53:50 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -23,7 +23,7 @@ end
 #4. Write a method that takes in the weather hash as an argument.
 # You'll want to use the each method to iterate over the hash and print out
 def weather_report (weather)
-	return "The weather in #{location($weather)} is {description($weather)}"
+	return "The weather in #{location($weather)} is #{description($weather)}"
 end
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
