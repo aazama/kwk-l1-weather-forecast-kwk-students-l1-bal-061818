c16d330de85af6d89cc57aaa992a699da6c72ff8
ava azama <avaazama@gmail.com>
2018-06-21 18:49:41 +0000

    automatically backed up by learn

--- weather.rb
+++ weather.rb
@@ -8,13 +8,13 @@ $weather = {
 
 # 1. Complete the method below called location, that accepts a hash called weather as an argument so that it returns the value of city from the weather hash.
 def location
-	return $weather [:city]
+	puts $weather [:city]
 
 end
 
 # 2. Using the location method as a guide, write a  method called "description" so it returns the description from the weather hash.
 def description
-	return $weather [:description]
+	puts $weather [:description]
 end
 
 # 3. Using the methods above and string interpolation, update the method below so that it returns a string that describes the current weather - for example: "The weather in New York is Partly Cloudy. The temperature is 66.1 degrees Farenheit." HINT: the method should RETURN the string, not print it to the terminal.
@@ -23,7 +23,7 @@ end
 #4. Write a method that takes in the weather hash as an argument.
 # You'll want to use the each method to iterate over the hash and print out
 def weather_report (weather)
-	return "The weather in #{location($weather)} is {description($weather)}"
+	puts "The weather in #{location($weather)} is {description($weather)}"
 end
 # `The city is New York.` `The description is Partly Cloudy.` `The temperature is 66.3.`
 
